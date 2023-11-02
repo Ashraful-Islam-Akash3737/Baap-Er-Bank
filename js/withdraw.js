2452ef6ba828ac34f441bf9a125031d988d86c42
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-11-03 01:48:30 +0600

    complete this project

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,6 +2,16 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawFeild = document.getElementById('withdraw-field');
     const withdrawAmountString = withdrawFeild.value;
     const withdrawAmount = parseFloat(withdrawAmountString);
+    
+
+    withdrawFeild.value = '';
+
+
+    
+    if(isNaN(withdrawAmount)){
+        alert("provide me a valid number");
+        return;
+    }
 
 
     const withdrawTotalAmount = document.getElementById('withdraw-total');
@@ -9,18 +19,23 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const totalAmount = parseFloat(withdrawTotalAmountString);
 
 
-    const newTotalAmount = totalAmount + withdrawAmount;
-    withdrawTotalAmount.innerText = newTotalAmount;
-
 
     const previousBalanceTotal = document.getElementById('balance-total');
     const previousBalanceTotalString = previousBalanceTotal.innerText;
     const newBalanceTotal = parseFloat(previousBalanceTotalString);
 
 
+    if(withdrawAmount > newBalanceTotal){
+        alert("Baap ar bank a ato taka nai");
+        return;
+    }
+    
+
+    const newTotalAmount = totalAmount + withdrawAmount;
+    withdrawTotalAmount.innerText = newTotalAmount;
+
+
     const currentBalanceTotal = newBalanceTotal  - withdrawAmount;
     previousBalanceTotal.innerText = currentBalanceTotal;
 
-
-    withdrawFeild.value = '';
 })
